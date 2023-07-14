290cf979c0464f7b4cd601b3b0b109ca8bdaf050
Leonardo Carmonio <leonardo.carmonio@gmail.com>
2023-07-14 19:05:56 -0300

    Adição do Docker no projeto.

--- back-end/sequelize.js
+++ back-end/sequelize.js
@@ -8,7 +8,8 @@ const db = new Sequelize(
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
-        dialect: "mysql"
+        dialect: "mysql",
+        port: process.env.DB_PORT
     }
 );
 
